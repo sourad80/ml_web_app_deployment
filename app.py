41f94ce8b81e9c381172d23bcef4c3718148e170
souradeep Guha Neogi <39113709+sourad80@users.noreply.github.com>
2020-07-02 14:35:58 +0530

    Add files via upload
    1st upload

---
+++ app.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score 
+
+def main():
+    st.sidebar.title("Binary Classification")
+    st.markdown("Is your mushroom poisnous?")
+    st.sidebar.markdown("Is your mushroom poisnous?")
+
+    @st.cache(persist = True)
+    def load_data():
+        data = pd.read_csv('D:\StreamlitProject\mushrooms.csv')
+        label = LabelEncoder()
+        for col in data.columns :
+            data[col] = label.fit_transform(data[col])
+        return data
+
+    @st.cache(persist = True)
+    def split(df):
+        y = df.type
+        x = df.drop(columns = ['type'])
+        x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3 , random_state = 0)
+        return x_train,x_test,y_train,y_test
+
+    def plot_metrics(metrics_list):
+        if 'Confusion Matrix' in metrics_list:
+            st.subheader("Confusuion Matrix")
+            plot_confusion_matrix(model,x_test,y_test,display_labels = class_names)
+            st.pyplot()
+        if 'ROC curve' in metrics_list:
+            st.subheader("ROC Curve")
+            plot_roc_curve(model,x_test,y_test)
+            st.pyplot()
+        if 'Precision-Recall Curve' in metrics_list:
+            st.subheader("Precision Recall Curve")
+            plot_precision_recall_curve(model,x_test,y_test)
+            st.pyplot()
+
+    df = load_data()
+    x_train,x_test,y_train,y_test = split(df)
+    class_names = ['edible','poisonous']
+
+
+    if st.sidebar.checkbox("Show data",False):
+        st.subheader("Mushroom Dataset")
+        st.write(df)
+
+    st.sidebar.subheader("Choose Classifier")
+    classifier = st.sidebar.selectbox("Classifier",("SVM","Logistic Regression","Random Forest"))
+    
+    if classifier == 'SVM':
+        st.sidebar.subheader("Input Hyperparameter")
+        c = st.sidebar.number_input("C (Regularization Parameter)",0.01,10.0,step = 0.01,key = 'C')
+        kernel = st.sidebar.radio("Kernel",("rbf","linear"),key = 'Kernel')
+        gamma = st.sidebar.radio("Gamma",("scale","auto"),key = 'gamma')
+
+        metrices = st.sidebar.multiselect("What metrices to plot?",('Confusion Matrix','ROC curve','Precision-Recall Curve'))
+
+        if st.sidebar.button("Clasify",key = 'clasify'):
+            st.subheader("Support Vector Machine Result")
+            model = SVC(C = c, kernel=kernel,gamma= gamma)
+            model.fit(x_train,y_train)
+            accuracy = model.score(x_test,y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precission :", precision_score(y_test,y_pred,labels=class_names).round(2))
+            st.write("Recall :", recall_score(y_test,y_pred,labels=class_names).round(2))
+            plot_metrics(metrices)
+    
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Input Hyperparameter")
+        c = st.sidebar.number_input("C (Regularization Parameter)",0.01,10.0,step = 0.01,key = 'C_lr')
+        max_iter = st.sidebar.slider("No. of Iterations",100,500,key = 'max_iter')
+
+        metrices = st.sidebar.multiselect("What metrices to plot?",('Confusion Matrix','ROC curve','Precision-Recall Curve'))
+
+        if st.sidebar.button("Clasify",key = 'clasify'):
+            st.subheader("Logistic Regression result")
+            model = LogisticRegression(C = c, max_iter= max_iter)
+            model.fit(x_train,y_train)
+            accuracy = model.score(x_test,y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precission :", precision_score(y_test,y_pred,labels=class_names).round(2))
+            st.write("Recall :", recall_score(y_test,y_pred,labels=class_names).round(2))
+            plot_metrics(metrices)
+
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Input Hyperparameter")
+        n_estimators = st.sidebar.number_input("N-Estimators",100,5000, step = 10, key = 'n_estimators')
+        max_depth = st.sidebar.number_input("Max Depth",1,20, step = 1, key = 'max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap when building trees",("True","False"), key = 'bootstrap')
+
+
+        metrices = st.sidebar.multiselect("What metrices to plot?",('Confusion Matrix','ROC curve','Precision-Recall Curve'))
+
+        if st.sidebar.button("Clasify",key = 'clasify'):
+            st.subheader("Logistic Regression result")
+            model = RandomForestClassifier(n_estimators= n_estimators,max_depth= max_depth,bootstrap= bootstrap,n_jobs= -1)
+            model.fit(x_train,y_train)
+            accuracy = model.score(x_test,y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", accuracy.round(2))
+            st.write("Precission :", precision_score(y_test,y_pred,labels=class_names).round(2))
+            st.write("Recall :", recall_score(y_test,y_pred,labels=class_names).round(2))
+            plot_metrics(metrices)
+            
+
+
+
+if __name__ == '__main__':
+    main()
+
+
